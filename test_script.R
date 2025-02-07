bf1de8cefb0c024510637600ec4cc3bc916602ca
s-minard <sam.minard.02@gmail.com>
2025-02-07 13:31:31 -0800

    added quarto document

---
+++ test_script.R
@@ -0,0 +1,3 @@
+usethis::git_sitrep()
+#package::function tells r to use the function directly from the specific package without having to load the entire package
+#also useful when multiple packages have the same function and you need to specify
